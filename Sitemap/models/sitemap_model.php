a9e7a1ac5e714e55dbcea3229bb01d8fed7a869f
Iskender <iskender@6ve1.com>
2011-04-18 02:35:18 -0700

    Edited Sitemap/models/sitemap_model.php via GitHub

--- Sitemap/models/sitemap_model.php
+++ Sitemap/models/sitemap_model.php
@@ -84,4 +84,59 @@ class Sitemap_model extends Base_model
                     return $tree;
                 }
         }
+
+        public function pagesXmlOutput()
+        {
+						$this->db->where(array('online' => 1, 'appears' => 1));
+						$this->db->order_by('ordering', 'DESC');
+            $pages	=   $this->db->get('page');
+
+            $xmlWr      =   '';
+            $xml        =   new XMLWriter();
+            $xmlWr      .=  $xml->openMemory();
+
+            $xmlWr      .=  $xml->startDocument('1.0', 'UTF-8');
+                $xmlWr      .=  $xml->startElement('urlset');
+                $xmlWr      .=  $xml->writeAttribute('xmlns', "http://www.sitemaps.org/schemas/sitemap/0.9");
+                $xmlWr      .=  $xml->writeAttribute('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance");
+                $xmlWr      .=  $xml->writeAttribute('xsi:schemaLocation', "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd");
+
+            foreach(Settings::get_languages() as $l)
+            {
+                foreach($pages->result_array() as $page):
+
+                                    $this->db->where(array('id_page' => $page['id_page'], 'lang' => $l['lang']));
+                    $pageLang   =   $this->db->get_where('page_lang');
+
+                    $pageL      =   $pageLang->row_array();
+
+                    $url        =   base_url().$l['lang'].'/'.$pageL['url'];
+
+                    $xmlWr      .=  $xml->startElement('url');
+                        $xmlWr      .=  $xml->startElement('loc');
+                            $xmlWr      .=  $xml->text($url);
+                        $xmlWr      .=  $xml->endElement();// end loc element
+                        $xmlWr      .=  $xml->startElement('changefreq');
+                            $xmlWr      .=  $xml->text(config_item('changefreq'));
+                        $xmlWr      .=  $xml->endElement();// end changefreq element
+                        //$xmlWr      .=  $xml->startElement('priority');
+                            //$xmlWr      .=  $xml->text(config_item('priority'));
+                        //$xmlWr      .=  $xml->endElement();// end priority element
+                        //$xmlWr      .=  $xml->startElement('lastmod');
+                            //$xmlWr      .=  $xml->text(config_item('lastmod'));
+                        //$xmlWr      .=  $xml->endElement();// end lastmod element
+                    $xmlWr      .=  $xml->endElement();// end url element
+
+
+                endforeach;
+
+            }
+
+                $xml->endElement();// end urlset element
+            $xml->endDocument();// end document
+
+            $xmlWr    .=  file_put_contents(config_item('file_path'), $xml->outputMemory());
+
+            return $xml;
+        }
 }
